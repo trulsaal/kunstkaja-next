901dfb2dbe72a1c076a79ccd90cf37dad1845008
Truls Løvik <trulsaal@gmail.com>
2025-02-12 09:09:35 +0100

    Created layout for gallery

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Header from "../Components/GalleryComponents/Header";
+import Sidebar from "../Components/GalleryComponents/SideBar";
 
-const Layout = ({ children }) => {
-  return <div className="">{children}</div>;
+interface Props {
+  children: any;
+}
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <div className="outline outline-2 outline-gray-400 w-lvw flex  flex-col">
+        <div className="flex">
+          <Header />
+        </div>
+        <div className="flex flex-row">
+          <Sidebar />
+
+          <div className="px-24 py-10">{children}</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Layout;
