64dc1e46bc568ad8ed3cd8f4864fa65cac3071ef
Truls Løvik <trulsaal@gmail.com>
2025-02-13 08:57:12 +0100

    Added aboutPage

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <div className="bg-blue-400 w-full m-auto">
+      <div>
+        <Image></Image>
+      </div>
+    </div>
+  );
+}
