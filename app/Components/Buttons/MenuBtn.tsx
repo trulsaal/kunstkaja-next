0b182fa440acea5a5cc0a131b010d0e4589cbd5c
Truls Løvik <trulsaal@gmail.com>
2025-02-13 08:46:25 +0100

    Changes to button css

--- app/Components/Buttons/MenuBtn.tsx
+++ app/Components/Buttons/MenuBtn.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Link from "next/link";
+
 interface Props {
   children: string;
   to: string;
 }
+
 export default function MenuBtn({ to, children }: Props) {
   return (
-    <Link href={to} className="min-w-fit max-w-full px-6 bg-none text-base">
+    <Link
+      href={to}
+      className="min-w-36 text-center  px-6 bg-none text-slate-900 text-lg font-semibold hover:font-thin hover:opacity-55 transition-all duration-200 h-fit"
+    >
       {children}
     </Link>
   );
