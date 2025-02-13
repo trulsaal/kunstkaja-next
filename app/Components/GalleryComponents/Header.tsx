a884ef11e1de17ef594c8105d20033bfcf22e197
Truls Løvik <trulsaal@gmail.com>
2025-02-13 20:31:55 +0100

    Tried out content for about-page

--- app/Components/GalleryComponents/Header.tsx
+++ app/Components/GalleryComponents/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="h-28 flex items-center">
-      <p className="tracking-tighter text-slate-700 text-5xl">
+      <p className="tracking-tighter text-slate-700 text-5xl ">
         <strong>kunst</strong>Kaja
       </p>
     </div>
