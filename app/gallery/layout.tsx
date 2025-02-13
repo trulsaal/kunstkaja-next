fddd9b8648529c61da2127750ad530f1fa78b814
Truls Løvik <trulsaal@gmail.com>
2025-02-13 08:45:23 +0100

    Changed size of div

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -8,14 +8,15 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <>
-      <div className="outline outline-2 outline-gray-400 w-lvw flex  flex-col">
+      <div className=" px-10 flex flex-col">
         <div className="flex">
           <Header />
         </div>
         <div className="flex flex-row">
           <Sidebar />
-
-          <div className="px-24 py-10">{children}</div>
+          <div className="flex justify-center w-full">
+            <div className="">{children}</div>
+          </div>
         </div>
       </div>
     </>
