a1e94021466c125b31afd8b93f1ffea02943d54b
Truls Løvik <trulsaal@gmail.com>
2025-02-14 09:52:42 +0100

    Minor changes to CSS and header

--- app/Components/HomeNav.tsx
+++ app/Components/HomeNav.tsx
@@ -4,11 +4,11 @@ export default function HomeNav() {
   return (
     <div className=" w-[800px] flex flex-col items-center justify-center gap-10 mt-auto h-3/4">
       <h1 className=" flex text-center text-[10rem] tracking-tighter text-slate-700">
-        <strong>kunst</strong>Kaja
+        <strong>kunst</strong>kaja
       </h1>
       <div className="w-fit  flex flex-row gap-5">
         <MenuBtn to="/gallery">til salgs</MenuBtn>
-        <MenuBtn to="">portefølje</MenuBtn>
+        <MenuBtn to="/portfolio">portefølje</MenuBtn>
         <MenuBtn to="/about">om</MenuBtn>
         <MenuBtn to="">kontakt</MenuBtn>
       </div>
