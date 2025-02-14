9a98bf1273a6f9d0aa544dac59921737440ab0c0
Truls Løvik <trulsaal@gmail.com>
2025-02-14 09:53:56 +0100

    Deleted documents not needed

--- app/Components/GalleryComponents/SideBar.tsx
+++ app/Components/GalleryComponents/SideBar.tsx
@@ -3,8 +3,8 @@ import SidebarBtn from "../Buttons/SidebarBtn";
 export default function Sidebar() {
   return (
     <div className="w-72 h-fit sticky top-24 grid grid-col gap-1 text-left">
-      <SidebarBtn to="/">til salgs</SidebarBtn>
-      <SidebarBtn to="/">portefølje</SidebarBtn>
+      <SidebarBtn to="/gallery">til salgs</SidebarBtn>
+      <SidebarBtn to="/portfolio">portefølje</SidebarBtn>
       <SidebarBtn to="/about">om</SidebarBtn>
       <SidebarBtn to="/">hjem</SidebarBtn>
     </div>
