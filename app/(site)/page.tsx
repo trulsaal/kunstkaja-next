9ee75156d04aa3f6c6e3c0bb084494ea82ab3a4f
Truls Løvik <trulsaal@gmail.com>
2025-02-16 21:53:27 +0100

    Making mobile-design

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ import HomeNav from "../Components/HomeNav";
 
 export default async function Home() {
   return (
-    <div className="w-lvw h-lvh flex flex-col items-center m-auto">
+    <div className="md:w-lvw h-lvh flex flex-col items-center m-auto">
       <HomeNav />
     </div>
   );
--- app/Components/HomeNav.tsx
+++ app/Components/HomeNav.tsx
@@ -2,17 +2,17 @@ import MenuBtn from "./Buttons/MenuBtn";
 
 export default function HomeNav() {
   return (
-    <div className=" w-[800px] flex flex-col items-center justify-center gap-10 mt-auto h-3/4">
-      <h1 className=" flex text-center text-[10rem] tracking-tighter text-slate-700">
+    <div className=" w-96 md:w-[800px] flex flex-col m-auto items-center justify-center  gap-5 md:gap-10 md:mt-auto md:h-3/4">
+      <h1 className="flex text-center md:text-[10rem] text-7xl tracking-tighter text-slate-700">
         <strong>kunst</strong>kaja
       </h1>
-      <div className="w-fit  flex flex-row gap-5">
+      <div className=" my-10 w-fit flex flex-col md:flex-row gap-3 md:gap-5">
         <MenuBtn to="/gallery">til salgs</MenuBtn>
         <MenuBtn to="/portfolio">portefølje</MenuBtn>
         <MenuBtn to="/about">om</MenuBtn>
         <MenuBtn to="">kontakt</MenuBtn>
       </div>
-      <div className="mt-auto mb-24">Icons</div>
+      <div className="md:mt-auto md:mb-24">Icons</div>
     </div>
   );
 }
