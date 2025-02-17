aa9ecff7b5ef676b452117808aee2232059a33a7
Truls Løvik <trulsaal@gmail.com>
2025-02-17 15:29:01 +0100

    lll

--- app/(site)/gallery/layout.tsx
+++ app/(site)/gallery/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Header from "../../Components/GalleryComponents/Header";
 import Sidebar from "../../Components/GalleryComponents/SideBar";
 
