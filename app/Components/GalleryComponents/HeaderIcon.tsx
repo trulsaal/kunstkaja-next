f9c18b345e169adb7c8390690ef7fe48fb729782
trulsaal <trulsaal@gmail.com>
2025-03-24 09:02:50 +0100

    Update HeaderIcon component to adjust className for smoother animation

--- app/Components/GalleryComponents/HeaderIcon.tsx
+++ app/Components/GalleryComponents/HeaderIcon.tsx
@@ -1,10 +1,10 @@
 import { CgArrowLeft } from "react-icons/cg";
 
 export default function HeaderIcon({
-  className = "translate-x-0 transition-all duration-500",
+  className = "translate-x-1 ease-out transition-all duration-500",
 }) {
   return (
-    <div className="flex flex-col justify-center text-4xl w-10">
+    <div className="flex flex-col justify-center text-4xl  w-10">
       <CgArrowLeft className={className} />
     </div>
   );
