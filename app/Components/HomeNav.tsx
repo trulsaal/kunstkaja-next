e39751a2f32161ded563b8e5ff4ad49ec24175f4
trulsaal <trulsaal@gmail.com>
2025-03-27 19:57:10 +0100

    Refactor HomeNav layout for improved structure and styling; update MenuBtn positioning for better alignment

--- app/Components/HomeNav.tsx
+++ app/Components/HomeNav.tsx
@@ -4,22 +4,28 @@ import SoMeIcons from "./SoMeIcons";
 
 export default function HomeNav() {
   return (
-    <div className="w-96 md:w-[800px] flex flex-col m-auto items-center justify-center gap-5 md:gap-10 md:h-3/4">
-      <div>
-        <p className=" text-center md:text-left text-3xl m-0  text-slate-700">
-          this is
-        </p>
-        <h1 className="flex text-left  leading-[50px] md:leading-[100px] m-0 md:text-[10rem] text-7xl tracking-tighter text-slate-700 dark:text-slate-100">
-          <strong> kunst</strong>kaja
-        </h1>
+    <div className=" w-96 md:w-[800px] flex flex-col m-auto items-center justify-center  md:h-3/4 ">
+      <div className="flex flex-col gap-16 md:gap-10  my-auto">
+        <div className="md:mb-16">
+          <p className=" text-center md:text-left text-3xl m-0 text-slate-700">
+            this is
+          </p>
+          <h1 className="flex text-left leading-[50px] md:leading-[100px] m-0 md:text-[10rem] text-7xl tracking-tighter text-slate-700 dark:text-slate-100">
+            <strong> kunst</strong>kaja
+          </h1>
+        </div>
+        <div className=" dark:text-slate-100 text-slate-500 font-bold w-full justify-center flex flex-col gap-3 md:gap-5">
+          <MenuBtn to="/gallery">til salgs</MenuBtn>
+          <MenuBtn to="/portfolio">portefølje</MenuBtn>
+          <MenuBtn to="/about">om</MenuBtn>
+        </div>
       </div>
-
-      <div className=" dark:text-slate-100 text-slate-700 my-10 w-fit flex flex-col md:flex-row gap-3 md:gap-5">
-        <MenuBtn to="/gallery">til salgs</MenuBtn>
-        <MenuBtn to="/portfolio">portefølje</MenuBtn>
-        <MenuBtn to="/about">om</MenuBtn>
+      <div
+        className="
+        w-fit flex mx-auto mt-auto"
+      >
+        <SoMeIcons />
       </div>
-      <SoMeIcons />
     </div>
   );
 }
