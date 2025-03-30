5ea8aef2c86402b93f32e9e16b36f8e38ab15f57
trulsaal <trulsaal@gmail.com>
2025-03-30 20:59:31 +0200

    Replace HomeImage component with background image in Home layout; refactor HomeNav for improved structure and styling

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,13 +1,19 @@
 export const revalidate = 0;
 
-import HomeImage from "../Components/HomeImage";
+import Image from "next/image";
 import HomeNav from "../Components/HomeNav";
 
 export default async function Home() {
   return (
-    <div className="md:w-lvw h-lvh flex flex-col md:flex-row items-center m-auto">
+    <div className="">
+      <Image
+        className="object-fill "
+        src="/kunstkajaBg.jpg"
+        alt="kunstkaja background image"
+        quality="100"
+        layout="fill"
+      />
       <HomeNav />
-      <HomeImage />
     </div>
   );
 }
--- app/Components/HomeNav.tsx
+++ app/Components/HomeNav.tsx
@@ -4,27 +4,23 @@ import SoMeIcons from "./SoMeIcons";
 
 export default function HomeNav() {
   return (
-    <div className=" w-96 h-[44rem] md:w-[800px] flex flex-col m-auto items-center justify-center  md:h-3/4 ">
+    <div className="w-96 opacity-95 h-[44rem] md:w-lvw flex flex-col m-auto items-center justify-center md:h-lvh">
       <div className="flex flex-col gap-16 md:gap-10  my-auto">
         <div className="md:mb-16">
-          <p className=" text-center md:text-left text-3xl m-0 text-slate-700">
-            this is
-          </p>
-          <h1 className="flex text-left leading-[50px] md:leading-[100px] m-0 md:text-[10rem] text-7xl tracking-tighter text-slate-700 dark:text-slate-100">
+          <h1 className="flex text-left  md:text-[10rem] text-7xl tracking-tighter  text-slate-100">
             <strong> kunst</strong>kaja
           </h1>
         </div>
-        <div className=" dark:text-slate-100 text-slate-500 font-bold w-full justify-center flex flex-col gap-3 md:gap-5">
+      </div>
+      <div className="flex w-full items-center justify-between pb-48 px-24">
+        <div className="  text-slate-950 font-bold w-fit justify-center flex flex-col gap-3 md:gap-5  text-2xl">
           <MenuBtn to="/gallery">til salgs</MenuBtn>
           <MenuBtn to="/portfolio">portefølje</MenuBtn>
           <MenuBtn to="/about">om</MenuBtn>
+          <div>
+            <SoMeIcons />
+          </div>
         </div>
-      </div>
-      <div
-        className="
-        w-fit flex mx-auto mt-auto"
-      >
-        <SoMeIcons />
       </div>
     </div>
   );
