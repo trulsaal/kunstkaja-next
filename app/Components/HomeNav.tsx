6980fa7e63b182657ebefb0c6e3c962b937f20f1
trulsaal <trulsaal@gmail.com>
2025-04-03 20:57:49 +0200

    Refactor HomeNav layout for improved spacing and structure; update text styles for better readability

--- app/Components/HomeNav.tsx
+++ app/Components/HomeNav.tsx
@@ -4,23 +4,23 @@ import SoMeIcons from "./SoMeIcons";
 
 export default function HomeNav() {
   return (
-    <div className="w-96 h-[44rem] md:w-lvw flex flex-col m-auto items-center justify-center md:h-lvh bg-red-200 ">
-      <div className="bg-green-200 flex w-fit flex-col gap-16 md:gap-0 md:h-fit my-auto justify-center items-center mb-32">
-        <h1 className="bg-blue-600 flex w-fit text-center md:text-[12rem] font-normal text-7xl md:leading-[130px] text-white md:m-0 md:p-0">
+    <div className="w-96  h-svh md:w-lvw flex flex-col m-auto items-center justify-center md:h-lvh gap-20 ">
+      <div className=" flex w-full flex-col gap-0 md:gap-0 md:h-fit mt-auto justify-center items-center">
+        <h1 className=" flex w-fit text-center md:text-[12rem] font-normal text-7xl md:leading-[130px] text-white md:m-0 md:p-0">
           kunstkaja
         </h1>
         <div>
-          <p className=" flex w-full text-8xl text-white opacity-60 md:leading-[100px] font-light text-left m-0 p-0">
+          <p className=" flex w-fit text-4xl text-white opacity-60 leading-[10px] md:leading-[100px] font-light text-left m-0 p-0">
             kaja andrea krøvel
           </p>
         </div>
       </div>
-      <div className="flex w-full items-center justify-between pb-48 px-24">
-        <div className="text-white md:text-slate-950 font-bold w-fit justify-center flex flex-col gap-3 md:gap-5 text-2xl">
+      <div className="flex w-full justify-between md:pb-48 px-24 mb-24 h-fit">
+        <div className="text-white md:text-slate-950 font-bold w-fit h-96 justify-center flex flex-col gap-5 text-2xl">
           <MenuBtn to="/gallery">til salgs</MenuBtn>
           <MenuBtn to="/portfolio">portefølje</MenuBtn>
           <MenuBtn to="/about">om</MenuBtn>
-          <div className="flex w-full justify-center md:block md:m-0">
+          <div className="flex w-full mt-auto justify-center md:block md:m-0 ">
             <SoMeIcons />
           </div>
         </div>
