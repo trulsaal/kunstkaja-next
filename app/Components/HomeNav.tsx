9a741cc71bc0218032c843569960971591b89071
trulsaal <trulsaal@gmail.com>
2025-04-03 21:07:14 +0200

    Refactor HomeNav component for improved layout consistency; adjust text styles and spacing for better readability

--- app/Components/HomeNav.tsx
+++ app/Components/HomeNav.tsx
@@ -4,19 +4,19 @@ import SoMeIcons from "./SoMeIcons";
 
 export default function HomeNav() {
   return (
-    <div className="w-96  h-svh md:w-lvw flex flex-col m-auto items-center justify-center md:h-lvh gap-20 ">
+    <div className=" w-96 h-svh md:w-lvw flex flex-col m-auto items-center justify-center md:h-lvh gap-20 md:gap-0">
       <div className=" flex w-full flex-col gap-0 md:gap-0 md:h-fit mt-auto justify-center items-center">
-        <h1 className=" flex w-fit text-center md:text-[12rem] font-normal text-7xl md:leading-[130px] text-white md:m-0 md:p-0">
+        <h1 className="flex w-fit text-center md:text-[12rem] font-normal text-7xl md:leading-[130px] text-white md:m-0 md:p-0">
           kunstkaja
         </h1>
         <div>
-          <p className=" flex w-fit text-4xl text-white opacity-60 leading-[10px] md:leading-[100px] font-light text-left m-0 p-0">
+          <p className="flex w-full text-4xl md:text-8xl text-white opacity-60 leading-[10px] md:leading-[100px] font-light text-left m-0 p-0">
             kaja andrea krøvel
           </p>
         </div>
       </div>
-      <div className="flex w-full justify-between md:pb-48 px-24 mb-24 h-fit">
-        <div className="text-white md:text-slate-950 font-bold w-fit h-96 justify-center flex flex-col gap-5 text-2xl">
+      <div className="flex w-full justify-between md:pb-48 px-24 mb-24 md:mb-0  h-fit">
+        <div className="text-white md:text-slate-950 font-bold w-fit h-96 justify-center flex flex-col gap-5 text-2xl  md:pt-44">
           <MenuBtn to="/gallery">til salgs</MenuBtn>
           <MenuBtn to="/portfolio">portefølje</MenuBtn>
           <MenuBtn to="/about">om</MenuBtn>
